d09f1621b9f5faac32f1c9122fdeb6edaf885dc1
ralucabrinceanu <brinceanu.raluca.alexandra@gmail.com>
2024-07-03 02:20:31 +0300

    express-rate-limit

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 const router = Router()
 
+import rateLimiter from 'express-rate-limit'
+
 import {
   register,
   login,
@@ -15,9 +17,15 @@ import {
 } from '../middleware/validation-middleware.js'
 import { authenticateUser } from '../middleware/auth-middleware.js'
 
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 15,
+  message: { msg: 'IP rate limit exceeded, retry in 15 minutes.' },
+})
+
 router.post('/register', validateRegisterInput, register)
 router.post('/verify-email', verifyEmail)
-router.post('/login', validateLoginInput, login)
+router.post('/login', apiLimiter, validateLoginInput, login)
 router.get('/logout', authenticateUser, logout)
 router.post('/reset-password', resetPassword)
 router.post('/forgot-password', forgotPassword)
